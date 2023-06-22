19a25314431b6fd800433f2b54e079555a234e53
Group Project <mala@Dreamville.com>
2023-06-22 15:31:36 +1000

    Group Capstone Project

---
+++ Project.py
@@ -0,0 +1,12 @@
+#Python__Weight Convertion
+
+weight = int(input('Weight '))
+unit = input('(L)bs or (K)g: ')
+
+
+if unit.upper() == "L":
+	converted = weight * 0.45
+	print(f"You are {converter} kilos")
+else: 
+	converted = weight / 0.45
+	print(f"You are {converted} pounds")
