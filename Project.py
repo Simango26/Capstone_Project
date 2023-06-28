86656e4cb1257685ef545048d079243a33feda0d
Simango26 <132625312+Simango26@users.noreply.github.com>
2023-06-28 13:13:15 +1000

    Update Project.py

--- Project.py
+++ Project.py
@@ -6,7 +6,7 @@ unit = input('(L)bs or (K)g: ')
 
 if unit.upper() == "L":
 	converted = weight * 0.45
-	print(f"You are {converter} kilos")
+	print(f"You are {converter} kilos"
 else: 
 	converted = weight / 0.45
 	print(f"You are {converted} pounds")
